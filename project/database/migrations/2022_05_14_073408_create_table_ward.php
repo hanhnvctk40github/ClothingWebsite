3518c13c4628ef17b6707dae9638aeb840c60e38
truonghoangngoclam <truong.lam@marketenterprise.vn>
2022-05-14 16:05:24 +0700

    [2]feat: add migrations, modal, routes, controller for client

---
+++ project/database/migrations/2022_05_14_073408_create_table_ward.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableWard extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ward', function (Blueprint $table) {
+            $table->string('xaid', 5);
+            $table->string('name', 100);
+            $table->string('type', 30);
+            $table->timestamps();
+            $table->primary('xaid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ward');
+    }
+}
